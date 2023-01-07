35782e1c9fa94e0bea74a0123cb325bbec9aa8a9
SifisoSithole <sitholesifisobrian@gmail.com>
2023-01-07 16:10:25 +0200

    Push: script

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,6 +3,7 @@
 Fabric script based on the file 2-do_deploy_web_static.py that creates and
 distributes an archive to the web servers
 """
+
 do_pack = __import__("1-pack_web_static").do_pack
 do_deploy = __import__("2-do_deploy_web_static").do_deploy
 
