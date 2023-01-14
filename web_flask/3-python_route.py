dd15ef80391df66d7366d0e8d4b1e015cc4ab0e1
SifisoSithole <sitholesifisobrian@gmail.com>
2023-01-14 12:21:02 +0200

    Add: 1-hbnb_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_cool(text):
+    """returns c <text>"""
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_is_cool(text='is_cool'):
+    """returns c <text>"""
+    return 'Python %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
