ddb8f76190564539eca33d1c5817b95c538f37ef
SifisoSithole <sitholesifisobrian@gmail.com>
2023-01-15 18:37:01 +0200

    Add: 1-hbnb_route.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,9 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def get_states():
-    """displays HTML oreded statesdds dssddsdsdsdssdds"""
+    """
+    displays HTML oreded statesdds dssddsdsdsdssdds
+    """
     states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
